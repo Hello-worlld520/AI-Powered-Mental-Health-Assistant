d4b9e6f2ea3fa929b18498cd7d40f06aa2751ffd
Hello-worlld520 <13199208595@163.com>
2026-09-03 12:35:34 +0800

    心理健康助手

---
+++ src/main/java/org/example/aipoweredmentalhealthassistant/AiPoweredMentalHealthAssistantApplication.java
@@ -0,0 +1,13 @@
+package org.example.aipoweredmentalhealthassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPoweredMentalHealthAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiPoweredMentalHealthAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/aipoweredmentalhealthassistant/AiPoweredMentalHealthAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aipoweredmentalhealthassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPoweredMentalHealthAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
